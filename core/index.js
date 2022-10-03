c70056bc237dba8d29920774f15347e94439fe78
Edd Gracilla <ed.gracilla@gmail.com>
2022-10-03 19:33:51 +0800

    init

---
+++ core/index.js
@@ -0,0 +1,37 @@
+/* eslint-disable security/detect-object-injection */
+
+const Joi = require('joi');
+const ApiError = require('./api-error');
+
+const pick = (object, keys) => keys.reduce((obj, key) => {
+  if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+    obj[key] = object[key];
+  }
+  return obj;
+}, {});
+
+const validate = (schema) => (req, res, next) => {
+  const validSchema = pick(schema, ['params', 'query', 'body']);
+  const object = pick(req, Object.keys(validSchema));
+
+  const { value, error } = Joi.compile(validSchema)
+    .prefs({ errors: { label: 'key' }, abortEarly: false })
+    .validate(object);
+
+  if (error) {
+    const errorMessage = error.details.map((details) => details.message).join(', ');
+    return next(new ApiError(400, errorMessage));
+  }
+
+  Object.assign(req, value);
+  return next();
+};
+
+const catchAsync = (fn) => (req, res, next) => Promise
+  .resolve(fn(req, res, next))
+  .catch((err) => next(err));
+
+module.exports = {
+  catchAsync,
+  validate,
+};
