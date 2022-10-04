5cd12c2fef838ef7ee2487ce0ac00edef04be74e
Edd Gracilla <ed.gracilla@gmail.com>
2022-10-04 08:31:18 +0800

    Update controller.js

--- src/controller.js
+++ src/controller.js
@@ -3,6 +3,7 @@
 const util = require('./utils');
 const svc = require('./service');
 
+const ApiError = require('../sys/api-error');
 const { parent } = require('../sys/binder');
 
 async function postSomething() {
@@ -15,15 +16,17 @@ async function getSomething(query) {
   const result = svc.getSomething();
   const capitalizedResult = util.capitalizeFirstLetter(result);
 
-  if (parent.logger) {
+  if (parent.logger && parent.logger.info) {
     parent.logger.info(parent.config);
   } else {
+    // eslint-disable-next-line no-console
     console.log(parent);
   }
 
-  if (query.foo) {
-    throw new Error('This will be handled by parent repo!');
-  }
+  // all errors will be handled by errHandler in recon-engine
+
+  if (query.normalErr) throw new Error('Unknown Err!');
+  if (query.apiErr) throw new ApiError(400, 'Some validation error!');
 
   return {
     message: `Something done in ${capitalizedResult} service!`,
